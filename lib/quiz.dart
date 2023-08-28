ec5abb7433487e7856fc039c24c6bb4247bb641c
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-08-28 16:25:01 +0700

    28/08/2023

---
+++ lib/quiz.dart
@@ -0,0 +1,48 @@
+import 'package:adv_basic_app/question_screen.dart';
+import 'package:adv_basic_app/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'question-screen';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //initialRoute: StartScreen.routeName,
+      // routes: {
+      //   StartScreen.routeName: (context) => const StartScreen(),
+      //   QuestionScreen.routeName: (context) => const QuestionScreen(),
+      // },
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionScreen(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  static const routeName = 'start-screen';
+
+  final Function onStart;
+  const StartScreen(this.onStart, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'lib/assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {
+              onStart();
+              //Navigator.pushNamed(context, QuestionScreen.routeName);
+            },
+            icon: const Icon(
+              Icons.arrow_right_alt,
+              color: Colors.white,
+            ),
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
