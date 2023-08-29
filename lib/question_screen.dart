5549ac046f59e9049c9505529909349b66d3e3ec
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-08-29 10:46:07 +0700

    29/08/2023

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,19 +1,67 @@
+import 'package:adv_basic_app/answer_button.dart';
+import 'package:adv_basic_app/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionScreen extends StatefulWidget {
   static const routeName = 'question-screen';
 
-  const QuestionScreen({super.key});
+  final Function(String) onSelectAnswer;
+  const QuestionScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
 
   @override
   State<QuestionScreen> createState() => _QuestionScreenState();
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+
+    setState(() {
+      ++currentQuestionIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('dhyfncudehf'),
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion
+                .getShuffledAnswers()
+                .map((answer) => AnswerButton(
+                      answerText: answer,
+                      onTap: () {
+                        answerQuestion(answer);
+                      },
+                    )),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basic_app/data/questions.dart';
 import 'package:adv_basic_app/question_screen.dart';
 import 'package:adv_basic_app/start_screen.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +11,24 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
       activeScreen = 'question-screen';
     });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
@@ -40,7 +53,7 @@ class _QuizState extends State<Quiz> {
           ),
           child: activeScreen == 'start-screen'
               ? StartScreen(switchScreen)
-              : const QuestionScreen(),
+              : QuestionScreen(onSelectAnswer: chooseAnswer),
         ),
       ),
     );
